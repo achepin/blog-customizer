4e092abf7932a481ef4befef396844a7edb0188d
Alexey <achepin@gmail.com>
2025-05-24 23:17:34 +0300

    выделение компонента App и оптимизация логики формы

---
+++ src/components/App/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+
+import { Article } from '../article/Article';
+import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
+import { defaultArticleState, ArticleStateType } from '../../constants/articleProps';
+import { useArticleCustomization } from '../../hooks/useArticleCustomization';
+
+import styles from '../../styles/index.module.scss';
+
+export const App = () => {
+	// Состояние только для примененных стилей статьи
+	const [articleStyles, setArticleStyles] = useState<ArticleStateType>(defaultArticleState);
+
+	const customStyles = useArticleCustomization(articleStyles);
+
+	// Единственный обработчик для применения стилей
+	const handleStylesApply = (newStyles: ArticleStateType) => {
+		setArticleStyles(newStyles);
+	};
+
+	return (
+		<main
+			className={clsx(styles.main)}
+			style={customStyles}>
+			<ArticleParamsForm onStylesApply={handleStylesApply} />
+			<Article />
+		</main>
+	);
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, useState } from 'react';
-import clsx from 'clsx';
+import { StrictMode } from 'react';
 
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState, ArticleStateType } from './constants/articleProps';
-import { useArticleCustomization } from './hooks/useArticleCustomization';
+import { App } from './components';
 
 import './styles/index.scss';
-import styles from './styles/index.module.scss';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	// Состояние только для примененных стилей статьи
-	const [articleStyles, setArticleStyles] = useState<ArticleStateType>(defaultArticleState);
-
-	const customStyles = useArticleCustomization(articleStyles);
-
-	// Единственный обработчик для применения стилей
-	const handleStylesApply = (newStyles: ArticleStateType) => {
-		setArticleStyles(newStyles);
-	};
-
-	return (
-		<main
-			className={clsx(styles.main)}
-			style={customStyles}>
-			<ArticleParamsForm onStylesApply={handleStylesApply} />
-			<Article />
-		</main>
-	);
-};
 
 root.render(
 	<StrictMode>
